b63ffea6e675cebd5a2710a07ecadde9ac9f14b7
minnieming <eunmilee9109.1@gmail.com>
2024-03-12 23:07:10 +0900

    [FEAT] 회원가입, 로그인 구현 (@builder, impl 수정 해야함)

---
+++ src/main/java/com/sparta/hanghaeboard/domain/user/dto/CheckResponseDto.java
@@ -0,0 +1,18 @@
+package com.sparta.hanghaeboard.domain.user.dto;
+
+import com.sparta.hanghaeboard.domain.user.entity.UserRoleEnum;
+
+public class CheckResponseDto {
+
+    private Long id;
+    private String nickname;
+
+    private UserRoleEnum role;
+
+
+    public CheckResponseDto(Long id, String nickname, UserRoleEnum role) {
+        this.id = id;
+        this.nickname = nickname;
+        this.role = role;
+    }
+}
